e81c89ff5b8e9ce8d8e34afc69f1a10c7608b5e6
Damilare Abiodun <dami@20ksites.com>
2021-03-30 08:40:36 +0100

    Base app state

---
+++ app/Models/Userdetail.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Userdetail extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'firstName','lastName','phone'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
